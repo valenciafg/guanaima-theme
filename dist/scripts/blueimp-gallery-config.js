44cb78bd0c433de116a2a9f5c75d7430655cc7fc
Victor Valencia <valencia6xgmail.com>
2016-04-07 16:35:30 -0430

    add dist directory

---
+++ dist/scripts/blueimp-gallery-config.js
@@ -0,0 +1,2 @@
+document.getElementById("links").onclick=function(e){e=e||window.event;var n=e.target||e.srcElement,t=n.src?n.parentNode:n,l={index:t,event:e},a=this.getElementsByTagName("a");blueimp.Gallery(a,l)};
+//# sourceMappingURL=blueimp-gallery-config.js.map
---
+++ dist/scripts/flexslider-config.js
@@ -0,0 +1,2 @@
+jQuery(document).ready(function(e){e(".flexslider").flexslider({animation:"slide"})});
+//# sourceMappingURL=flexslider-config.js.map
